5e97d2bfb6c74e2536e5fb2987efd04ee71c7c84
michel <michel@debian>
2019-04-05 00:18:46 -0300

    Created project using spring boot

---
+++ balancedBrackets/src/main/java/br/com/bravi/balancedBrackets/BalancedBracketsApplication.java
@@ -0,0 +1,13 @@
+package br.com.bravi.balancedBrackets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalancedBracketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalancedBracketsApplication.class, args);
+	}
+
+}
